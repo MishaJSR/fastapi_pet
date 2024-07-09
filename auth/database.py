12eb2917602f53f59b03fbc2f94a809fcd9e1125
Michail S <mixan_45@mail.ru>
2024-07-09 17:21:49 +0300

    1.0.2

--- auth/database.py
+++ auth/database.py
@@ -1,19 +1,30 @@
+from datetime import datetime
 from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped
+from config import SQLALCHEMY_DATABASE_URI
+from sqlalchemy import Boolean, ForeignKey, TIMESTAMP, Integer, String, func, select, Column
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+DATABASE_URL = SQLALCHEMY_DATABASE_URI
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    email: str = Column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
 
 
 engine = create_async_engine(DATABASE_URL)
